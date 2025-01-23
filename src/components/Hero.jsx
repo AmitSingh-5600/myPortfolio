d137562aec2874bc84ca9e2415df7ef0262646e7
Amit Singh Kushwaha <136868174+AmitSingh-5600@users.noreply.github.com>
2025-01-23 22:08:14 +0530

    some changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,33 +3,35 @@ import { FaGithubSquare, FaLinkedin } from "react-icons/fa";
 
 function Hero() {
   return (
-    <section className="bg-emerald-100 grid sm:grid-cols-2 gap-8 px-8 py-4">
-      <article className="mx-auto w-full">
-        <h1 className="text-4xl font-bold">I&apos;m Amit Singh</h1>
-        <h2 className="text-2xl mt-4 font-bold text-slate-500 ">
-          Full-Stack Developer
-        </h2>
-        <p className="text-1xl mt-2 font-bold text-slate-500">
-          &quot;Simplicity is my lens to uncover the essence of
-          complexity.&quot;
-        </p>
-        <div className="flex mt-3 gap-4">
-          <a href="https://github.com/AmitSingh-5600" target="_blank">
-            <FaGithubSquare className="h-8 w-8  text-slate-500 hover:text-black" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/amit-singh5600/"
-            target="_blanck"
-          >
-            <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-black" />
-          </a>
-        </div>
-      </article>
+    <div className="bg-emerald-100">
+      <section className="grid sm:grid-cols-2 gap-8 p-8 max-w-7xl mx-auto">
+        <article className="mx-auto w-full">
+          <h1 className="text-4xl font-bold">I&apos;m Amit Singh</h1>
+          <h2 className="text-2xl mt-4 font-bold text-gray-600">
+            Full-Stack Developer
+          </h2>
+          <p className="text-lg mt-2 font-medium text-gray-600">
+            &quot;Simplicity is my lens to uncover the essence of
+            complexity.&quot;
+          </p>
+          <div className="flex mt-3 gap-4">
+            <a href="https://github.com/AmitSingh-5600" target="_blank">
+              <FaGithubSquare className="h-8 w-8 text-gray-600 hover:text-black" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/amit-singh5600/"
+              target="_blank"
+            >
+              <FaLinkedin className="h-8 w-8 text-gray-600 hover:text-black" />
+            </a>
+          </div>
+        </article>
 
-      <article className=" w-1/1  hidden sm:block">
-        <img src={hero} className="size-52" />
-      </article>
-    </section>
+        <article className="hidden sm:block">
+          <img src={hero} alt="Hero Image" className="w-full h-60" />
+        </article>
+      </section>
+    </div>
   );
 }
 
